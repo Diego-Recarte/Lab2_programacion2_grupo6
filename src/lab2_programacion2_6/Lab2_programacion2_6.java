5197a05b515c08b84a8abfb6c25e88b17f946b37
DiegoR <diegoerecarte@gmail.com>
2026-08-01 10:32:34 -0600

    fix: merge conflict in main

--- src/lab2_programacion2_6/Lab2_programacion2_6.java
+++ src/lab2_programacion2_6/Lab2_programacion2_6.java
@@ -4,11 +4,9 @@ package lab2_programacion2_6;
 public class Lab2_programacion2_6 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
        FrameGestion ventana=new FrameGestion(); 
-=======
+
         
->>>>>>> desarrollo-Bella
     }
     
 }
