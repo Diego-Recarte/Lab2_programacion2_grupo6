4dc6957edeeb72c7f2b1a2e21ea7e5345ea1122a
bell0-0-dot <bella_serrano@unitec.edu>
2026-08-01 10:41:21 -0600

    Merge--Bella

--- src/lab2_programacion2_6/Lab2_programacion2_6.java
+++ src/lab2_programacion2_6/Lab2_programacion2_6.java
@@ -4,11 +4,11 @@ package lab2_programacion2_6;
 public class Lab2_programacion2_6 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
        FrameGestion ventana=new FrameGestion(); 
-=======
+
         
->>>>>>> desarrollo-Bella
+
     }
     
 }
