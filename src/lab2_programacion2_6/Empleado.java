0f9b9e2709710aaf09821963941915d011f8dbd3
Fercho <fernandoaltamirano.vdc@gmail.com>
2026-08-01 11:34:24 -0600

    arreglando cosas

--- src/lab2_programacion2_6/Empleado.java
+++ src/lab2_programacion2_6/Empleado.java
@@ -43,7 +43,7 @@ public class Empleado {
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Ingrese horas válidas.");
-                leer.next(); // Limpia el buffer del Scanner correctamente
+                leer.next(); 
             }
         } while (!horasAceptadas);
         return this.horasTrabajadas;
@@ -54,7 +54,7 @@ public class Empleado {
             System.out.println("Horas sobrepasan el limite. Se calcula con el maximo de 160hrs.");
             horas = 160;
         }
-        double subtotal = (horas / 160.0) * salarioBase; // 160.0 para evitar división entera
+        double subtotal = (horas / 160.0) * salarioBase;
         double deduccion = salarioBase * 0.035;
         return subtotal - deduccion;
     }
